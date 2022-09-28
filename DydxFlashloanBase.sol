e224264ec702373f755625a13296a28442f79469
Parmendra1 <114565273+Parmendra1@users.noreply.github.com>
2022-09-28 15:22:48 +0530

    Update DydxFlashloanBase.sol

--- DydxFlashloanBase.sol
+++ DydxFlashloanBase.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.7;
+pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./IERC20.sol";
+import "./SafeMath.sol";
 
 import "./ISoloMargin.sol";
 
