b1e2649ab4f0098c64be6fc73e04dc27c86f10e6
Parmendra1 <114565273+Parmendra1@users.noreply.github.com>
2022-09-28 14:29:37 +0530

    Update ICallee.sol

--- ICallee.sol
+++ ICallee.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.8.0;
 pragma experimental ABIEncoderV2;
 
-import { Account } from "./Account.sol";
+import { Account } from "github.com/Parmendra1/Dydx/blob/main/Account.sol";
 
 
 /**
